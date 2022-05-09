c883daa27bfa2cfe1a60f01b091bc6cdd368ac80
Gabrielle Guimarães de Oliveira <gabrielle1guim@gmail.com>
2022-05-09 08:44:06 -0300

    Implement `ExposedFoodRepository.findFood`

---
+++ src/jvmTest/kotlin/food/infra/ExposedFoodRepositoryTest.kt
@@ -0,0 +1,59 @@
+/*
+ * CREME application
+ * Copyright (C) 2022  Gabrielle Guimarães de Oliveira
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package creme.apply.food.infra
+
+import creme.apply.withTestDB
+import kotlinx.coroutines.runBlocking
+import org.jetbrains.exposed.sql.insertAndGetId
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.jupiter.api.Test
+import java.util.UUID
+import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertNull
+
+class ExposedFoodRepositoryTest {
+  @Test
+  fun `test should return null when database does not contains it`(): Unit = withTestDB {
+    val foodRepository = ExposedFoodRepository()
+
+    val recipe = runBlocking { foodRepository.findFood(UUID.randomUUID().toString()) }
+
+    assertNull(recipe)
+  }
+
+  @Test
+  fun `test should return a food when database contains it`(): Unit = withTestDB {
+    val foodRepository = ExposedFoodRepository()
+
+    val foodEntityId = transaction {
+      FoodTable.insertAndGetId {
+        it[name] = "Alho poró"
+        it[hero] = "..."
+      }
+    }
+
+    val recipe = runBlocking { foodRepository.findFood(foodEntityId.value.toString()) }
+
+    assertNotNull(recipe)
+    assertEquals(foodEntityId.value.toString(), recipe.id)
+    assertEquals("Alho poró", recipe.name)
+    assertEquals("...", recipe.hero)
+  }
+}
